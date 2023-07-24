0c227be51ae31c92a65c1deaf69adf528dc72ddd
ssmmrr <septr_4022@naver.com>
2023-07-24 11:35:51 +0900

    test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 fetch('http://oreumi.appspot.com/video/getVideoList')
-    .then(res => res.json()) // .json() 메서드는 JSON 응답을 JavaScript 객체 리터럴로 구문분석합니다.
+    .then(res => res.json())
     .then(data => console.log(data));
 
