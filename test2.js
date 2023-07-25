498d35c470dc9e041875f6e033115c741c0ab70b
giyong choi <137133519+giyong-choi@users.noreply.github.com>
2023-07-25 15:37:48 +0900

    Update test2.js

--- test2.js
+++ test2.js
@@ -4,18 +4,20 @@ fetch('http://oreumi.appspot.com/video/getVideoList')
 
 
 
-/* 3페이지 HTML에 추가 <button id="subscribe-button" style="background-color: red;">
-Subscribe</button>*/
+/* 3페이지 HTML에 추가 <button id="subscribe-button" style="background-color: #cc0000; 
+color: white;">SUBSCRIBES</button>*/
+
+
 
 const subs_Btn = document.getElementById('subscribe-button');
 
 subs_Btn.addEventListener('click', function(b) {
-    if(subs_Btn.innerText === 'Subscribe'){
-        subs_Btn.innerText = '1 Subscribed';
+    if(subs_Btn.innerText === 'SUBSCRIBES'){
+        subs_Btn.innerText = '1 SUBSCRIBED';
         b.target.style.backgroundColor = 'darkgray';
     } else {
-        subs_Btn.innerText = 'Subscribe';
-        b.target.style.backgroundColor = 'red';
+        subs_Btn.innerText = 'SUBSCRIBES';
+        b.target.style.backgroundColor = '#cc0000';
     }});
 
 /* 별다른 기능없이 클릭 시에 버튼색과 텍스트만 반복적으로 변경되게 했습니다. */
