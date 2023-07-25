5543a4445708807c7ff13411116e347adb79be73
giyong choi <137133519+giyong-choi@users.noreply.github.com>
2023-07-25 17:04:42 +0900

    Update test2.js

--- test2.js
+++ test2.js
@@ -4,11 +4,24 @@ fetch('http://oreumi.appspot.com/video/getVideoList')
 
 
 
-/* 3페이지 HTML에 추가 <button id="subscribe-button" style="background-color: #cc0000; 
-color: white;">SUBSCRIBES</button>*/
+/* 구독 버튼입니다. 2,3페이지에 사용되며 피그마에 나와있는 크기와 색상대로 하였고 구독 시에 변경되는 버튼의 색은 따로 나와있지 않아서 임의로 회색으로 했습니다.
+HTML 및 CSS에 추가하면 될 거 같습니다.*/
 
-
-
+ /*HTML에 
+ <button id="subscribe-button">SUBSCRIBES</button> 추가*/
+ 
+ /*CSS에  
+ #subscribe-button {
+    background-color: #cc0000; 
+    color: #fff;
+    display: flex; 
+    padding: 10px 16px; 
+    align-items: flex-start; 
+    gap: 10px; 
+    font-size: 14px; 
+    font-weight: 700;
+ } 추가*/
+ 
 const subs_Btn = document.getElementById('subscribe-button');
 
 subs_Btn.addEventListener('click', function(b) {
@@ -19,5 +32,3 @@ subs_Btn.addEventListener('click', function(b) {
         subs_Btn.innerText = 'SUBSCRIBES';
         b.target.style.backgroundColor = '#cc0000';
     }});
-
-/* 별다른 기능없이 클릭 시에 버튼색과 텍스트만 반복적으로 변경되게 했습니다. */
