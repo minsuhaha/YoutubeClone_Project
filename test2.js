0d739cdfef4ede93a9702d1226dc4c8063fddfa2
giyong choi <137133519+giyong-choi@users.noreply.github.com>
2023-07-25 15:03:38 +0900

    Update test2.js
    구독버튼 기능만 추가하였습니다. 강의 종료까지 시간이 된다면 댓글 기능까지 추가하겠습니다.

--- test2.js
+++ test2.js
@@ -2,3 +2,20 @@ fetch('http://oreumi.appspot.com/video/getVideoList')
     .then(res => res.json())
     .then(data => console.log(data));
 
+
+
+/* 3페이지 HTML에 추가 <button id="subscribe-button" style="background-color: red;">
+Subscribe</button>*/
+
+const subs_Btn = document.getElementById('subscribe-button');
+
+subs_Btn.addEventListener('click', function(b) {
+    if(subs_Btn.innerText === 'Subscribe'){
+        subs_Btn.innerText = '1 Subscribed';
+        b.target.style.backgroundColor = 'darkgray';
+    } else {
+        subs_Btn.innerText = 'Subscribe';
+        b.target.style.backgroundColor = 'red';
+    }});
+
+/* 별다른 기능없이 클릭 시에 버튼색과 텍스트만 반복적으로 변경되게 했습니다. */
