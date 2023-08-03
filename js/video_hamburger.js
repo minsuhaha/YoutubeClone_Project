96b831e8a3e3000f50529f3d00659ec9d5607260
minsuhaha <haminsu5@gmail.com>
2023-08-03 14:20:51 +0900

    video 페이지 햄버기 기능 수정

--- js/video_hamburger.js
+++ js/video_hamburger.js
@@ -1,26 +1,13 @@
-let sidebarHidden = false;
-    // const allowedLinkNames = ["Home", "Explore", "Subscriptions", "Library", "History", "Your Videos", "watch Later"];
-    function toggleSidebar() {
-        const sidebarLinks = document.querySelectorAll('.sidebar> div > div');
-        sidebarHidden = !sidebarHidden;//버튼 눌러서 숨김으로 반전
-        const sidebar = document.querySelector('.sidebar');
-        if (sidebarHidden) {
-        sidebar.classList.add('sidebarHidden_vd');
-        } else {
-        sidebar.classList.remove('sidebarHidden_vd');
+let sidebarVisible = false;
+
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebarVisible = !sidebarVisible;
+    if (sidebarVisible) {
+        sidebar.style.left = "0px";
+    } else {
+        sidebar.style.left = "-240px";
     }
-
-        sidebarLinks.forEach((link) => {
-            const linkname = link.querySelector('.sidebar')
-
-            // if (allowedLinkNames.includes(linkname)) { //무조건 보일 요소들 집합에 linkname이 있으면 block
-            //     link.style.display = 'block';
-            // }
-            if (sidebarHidden) {// sidebar 상태가 숨겨져 있는걸로 설정 되어있으면 none
-                link.style.display = 'none';
-            }
-            else {//sidebar 상태가 보이는 걸로 설정 되어있으면 block
-                link.style.display = 'block';
-            }
-        });
 }
+
+document.getElementById('sidebarToggle').addEventListener('click', toggleSidebar);
