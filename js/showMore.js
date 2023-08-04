d7bdd3bcde57d7a76f2f4460579f692cd380a7c6
minsuhaha <haminsu5@gmail.com>
2023-08-04 13:48:57 +0900

    배포 전 main 수정

--- js/showMore.js
+++ js/showMore.js
@@ -4,13 +4,13 @@ document.getElementById('show-more').addEventListener('click', function (e) {
     let arrowImg = document.getElementById('arrow-img');
     if (hiddenPart.style.display == 'none') {
         hiddenPart.style.display = 'block';
-        arrowImg.src = '../Image/Sidebar/chevron-up-solid.svg';
+        arrowImg.src = './Image/Sidebar/chevron-up-solid.svg';
         arrowImg.style.width = '15px';
         arrowImg.style.height = '30px';
         arrowImg.style.color = '#ffffff';
     } else {
         hiddenPart.style.display = 'none';
-        arrowImg.src = '../Image/Sidebar/show.svg';
+        arrowImg.src = './Image/Sidebar/show.svg';
         arrowImg.style.width = '15px';
         arrowImg.style.height = '15px';
     }
